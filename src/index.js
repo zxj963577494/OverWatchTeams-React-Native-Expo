713c28238d103a210d692648eeaa5f652f1894c6
Xujiang Zheng <963577494@qq.com>
2017-12-26 15:10:12 +0800

    整理

--- src/index.js
+++ src/index.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react'
 import { Provider, connect } from 'react-redux'
 import { addNavigationHelpers } from 'react-navigation'
+import { AppLoading, Asset, Font } from 'expo'
+import { Ionicons } from '@expo/vector-icons'
 import Navigator from './routes'
 import configureStore from './store'
 import rootSaga from './sagas'
 
-const App = ({ dispatch, nav }) => (
-  <Navigator
-    navigation={addNavigationHelpers({
-      dispatch,
-      state: nav
-    })}
-  />
-)
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image)
+    } else {
+      return Asset.fromModule(image).downloadAsync()
+    }
+  })
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font))
+}
+
+class App extends Component {
+  state = {
+    isReady: false
+  }
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([
+      require('../assets/images/home.png'),
+      require('../assets/images/homeIcon.png'),
+      require('../assets/images/avatarLogo.png')
+    ])
+    const fontAssets = cacheFonts([Ionicons.font])
+    await Promise.all([...imageAssets, ...fontAssets])
+  }
+
+  render() {
+    const { dispatch, nav } = this.props
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      )
+    }
+    return (
+      <Navigator
+        navigation={addNavigationHelpers({
+          dispatch,
+          state: nav
+        })}
+      />
+    )
+  }
+}
 
 const mapStateToProps = state => ({
   nav: state.nav
